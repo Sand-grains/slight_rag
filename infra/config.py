992d4e32307939842b6c32e70515a9c5547c1f44
bkv <2298581658@qq.com>
2026-07-28 15:22:21 +0800

    修改RedisTTL

--- infra/config.py
+++ infra/config.py
@@ -3,4 +3,4 @@ from config import _PROJECT_ROOT
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 REDIS_KEY_PREFIX = os.getenv("REDIS_KEY_PREFIX", "slight_rag")
-REDIS_DEFAULT_TTL = int(os.getenv("REDIS_DEFAULT_TTL", "86400"))  # 24h
+REDIS_DEFAULT_TTL = int(os.getenv("REDIS_DEFAULT_TTL", "259200"))  # 72h
